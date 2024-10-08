d09e0d264e7010227fe0779aaec7e50daa763ffe
Theliftrobot <mcc@theliftco.eu>
2024-10-09 02:49:31 +0300

    Update updts.py

--- dgbt/scripts/updts.py
+++ dgbt/scripts/updts.py
@@ -17,6 +17,7 @@ def gauth(creds):
 def check_clear(acc, client):
     sheet_id = "1Gi-bTqVdQWG1B5bUZvvz2_798a8HHdh6UZ-nujZ2L0I"
     spreadsheet = client.open_by_key(sheet_id)
+    print(spreadsheet)
     try:
         worksheet = spreadsheet.worksheet(acc)
         worksheet.clear()
@@ -50,16 +51,32 @@ def add_rows(row_dict, worksheet, client, acc):
 
 class MembsSpider(scrapy.Spider):
     name = "membs"
-    data = pkgutil.get_data("dgbt","resources/creds.json")
-    creds = json.loads(data.decode('utf-8'))
-    client = gauth(creds)
+    # data = pkgutil.get_data("dgbt_main","dgbt/resources/creds.json")
+    # creds = json.loads(data.decode('utf-8'))
+    creds = {
+        "type": "service_account",
+        "project_id": "accountssheets-438006",
+        "private_key_id": "43368ee81614933c3d37099c15c8c6c641054381",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDU/i+tBFgr6a8h\nvZeBZLwdRoGUTqOmE2SvUZv3wkgOv++JcTSx7ofZX0w+tFu9MYnlL6AghOBFqnn6\nv+Z5V1aliYkli4YpqceiWltetSFUH5WXbn4kJrfKh2aXkvpU2C3qUQtkFfsQWEsV\niPr26dHR03BBv5mjLEc5GigjZFafK1kb2hm6T6GxyoiySQjVnCwX0QLnb+LR2Cqr\n9LBxQrfaBNVzjx5n3Dx5u1cpYLe1mqF2MElWJA+20mpq/yeH82A/CI3tRMYgg46Z\nQ6gu99d6VoABFZVP/ZR9hBu0nGMlsFIN+1ziTjMSW9Gluu13jpsF4uKRjmeXy08P\nTwu2PsVDAgMBAAECggEAUv/Mj1USkOYq33CUshERtyzLAQKWBfknsqQQLb+xOcI0\ns3AAc1f660uWGBdLapH75OomsZVmGe/BQeP3CZDtzGsonQ9eVCz7hPpGAcV+u9vk\n/NJLaIYH9+3Enkthrp9hYR583F8ua2Okursa5Q4fu+7zn0NtOOdfx9I4EvaeRL62\n52D38X6Ql6vGVnLxsbbl64mH6Cqd+nfs0/k5eOYPNgkt6kDBewrrqAHgMW4KSwBF\n3y4CHvv+h2paD4JQOaDUroxAaUFPK5pF3zbpWVqQAwCAntOq4uQvwf9KNUQxGnK8\ndh5zgYc3j6WzuSEkO5N0zUHOyb0Q6OyX5wGKbs6gdQKBgQD2NkCaQgmcUMoQXlCh\n06KpQ91lT72hO4ifVnm/eXgdZoLYQlz2K/b+CUprvEgyMoJqhbtJnrd01tQTzqT9\nsKd8BA+0Sy7i/0TJcn611ZOnGsf6K7w5VTcwe+yrSxAxrGBwi46vbGfyNHZO9GkJ\n8Hh+pHiqu9PnVRMuoVPjCGJXnwKBgQDddduA1qZxGGdctFQ4tbxOw3UeBOS2AYd9\nnzoUvNWfqvO6TqKLeXG/3GFM07wq3SkJSkOTwJCmxPrUg71E6V/TiVYDrJA/NkLh\nWCPdM2LNTfv/42jzGh/iV8czqSlVAgHMmcbIZTy3F5euCdlSDQ+zobpzTWHm1QaT\nrjS5CZc/3QKBgQDTyMT3K7jCmgfF3qrzGG2BtduaCuZt6xzRGnxtRJoaiBQi26rF\nIfo6eFlopLNhYmsmH9SDNiBOQ4B3bTgk9DCND51Gk6lIHxXXRkJSPN3eTr4Xpkko\n8/EeJmrkyROr9r+Z76GTqecbwx7FOZ2krBGpteYJzrku8tImOSVEU/DrtwKBgQC2\n1wNvD8lDPn64DzopyAG8laswKaIakpCmqrttO6qztJSdkSaqOI4tdWnv4DBOw2GK\nBgJdDnNe/OqKYmn1ZyhyocSeK+68AbSeEAMsMay1DFmuHrcbXspMOWSBxnwVbx7F\nKYDxGoRSNexJCGCgWaBJEpG5eH4H8oEHlKB61OxHEQKBgBvEFLQqaN2Whisc4O4T\nSL2YjPMzWvdBqVaVozQ5zXR6lWrgDNg3tPdX8g95OGwOpyDkk9Pa1+WG4AnNiirl\nzZ39epkAKmhQK18mT8zv2UpUynY12itV9CEgctxTGck6FkYcsyVg789RQMn+XN/W\npPFpzZDxNCzu5A6nNWqtD5Zi\n-----END PRIVATE KEY-----\n",
+        "client_email": "gsheets-updates@accountssheets-438006.iam.gserviceaccount.com",
+        "client_id": "105941998225468511415",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/gsheets-updates%40accountssheets-438006.iam.gserviceaccount.com",
+        "universe_domain": "googleapis.com"
+        }
 
+
+    CLIENT = gauth(creds)
+    print(CLIENT)
+    
     def start_requests(self):
         
         data = pkgutil.get_data("dgbt","resources/accounts.csv")
         df = pd.read_csv(BytesIO((data)))
-        
-            
+        # df = pd.read_csv(r'D:\Coding\Scrapping\Scrapy_projects\Freelancing\28-Oscar-dgpt\dgbt_main\dgbt\resources\accounts.csv')
+          
         for index, row in df.iterrows():
             account = str(row['account'].strip())
             password = str(row['password'].strip())
@@ -100,12 +117,11 @@ class MembsSpider(scrapy.Spider):
             
             
     def parse(self, response):
-    
             res_body = json.loads(response.text)
             print(res_body)
             account = response.meta['account']
             token = res_body.get('data').get('token')
-            worksheet = check_clear(account, self.client)
+            worksheet = check_clear(account, self.CLIENT)
             membs_url = 'https://api.dgpt.club/api/myteam/mylist?lang=en&v=1.3.5&page=1&grade=&isvalid=&dateline=all&level='
             headers = {
                     'Accept': 'application/json, text/plain, */*',
@@ -195,7 +211,7 @@ class MembsSpider(scrapy.Spider):
             }
         
         print(worksheet)
-        add_rows(row_dict, worksheet, self.client, account)
+        add_rows(row_dict, worksheet, self.CLIENT, account)
 
         yield{
             'Membro': name,
@@ -207,7 +223,7 @@ class MembsSpider(scrapy.Spider):
         
         
 process = CrawlerProcess({
-    'RETRY_TIMES' : 10,  
+    'RETRY_TIMES' : 5,  
 
     'RETRY_HTTP_CODES' : [500, 502, 503, 504, 522, 524, 408, 400, 401, 402, 403, 404, 405, 406, 429],
 
@@ -222,7 +238,7 @@ process = CrawlerProcess({
     
     'AUTOTHROTTLE_ENABLED': True,
     'AUTOTHROTTLE_START_DELAY' : 5,
-    'AUTOTHROTTLE_MAX_DELAY' : 10,
+    'AUTOTHROTTLE_MAX_DELAY' : 15,
     'DOWNLOAD_DELAY' : 3,
 
     'LOG_LEVEL' : 'DEBUG',
